f25cdd42662a5a95d03a2a8c27eccf06dd529faf
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-02-08 15:41:15 +0000

    first test method completed

--- bowling.rb
+++ bowling.rb
@@ -2,4 +2,8 @@ class Bowling
     def do_something(thing)
         thing
     end
+
+    def take_bowl()
+
+    end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../bowling'
 
 describe Bowling do
-    it "does something" do
+    it "takes 1 strike, scores 10" do
         # Arrange
         bowling = described_class.new
 
         # Act
-        result = bowling.do_something(1)
+        result = bowling.take_bowl(["X"])
 
         # Assert
-        
+        expect(result).to eq(10)
     end
 end
