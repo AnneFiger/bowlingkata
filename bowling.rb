b10df54a828cbd621bda5578f181478edb8a97a2
Anne Figer <anne-laure.figer@madetech.com>
2022-02-10 12:26:07 +0000

    worked on method to incorporate more than one frame

--- bowling.rb
+++ bowling.rb
@@ -1,18 +1,26 @@
-class Bowling    
-def get_score(roll)
-    if roll.length == 1
-        return "10"
-    
-    else 
-        if roll[1] == "/"
-            score = 10.to_s
-            return score
-        else 
-            first_number = roll[0].to_i
-            second_number = roll[1].to_i
-            final_score = (first_number + second_number).to_s
-            return final_score  
-        end
-    end    
-end
+class Bowling
+    # def input_to_frames(abc)
+    #     return abc.split(" ").to_s
+    # end
+
+
+    def get_score(input)
+        separate_frames = input.split(" ")
+        result_for_frame = 0
+        for frame in separate_frames
+            if frame.length == 1
+                result_for_frame += 10
+            else 
+                if frame[1] == "/"
+                    result_for_frame += 10
+                else 
+                    first_number = frame[0].to_i
+                    second_number = frame[1].to_i
+                    frame_score = (first_number + second_number)
+                    result_for_frame += frame_score
+                end
+            return result_for_frame   
+            end   
+        end    
+    end
 end
